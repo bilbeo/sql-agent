199df9d31aba27d16c1e87e4450d616ccad730a9
sat-m <markosyansatenik@gmail.com>
2019-04-25 16:53:31 +0400

    publish,autoupdate, for sincle instance

--- main.ts
+++ main.ts
@@ -1,7 +1,44 @@
-import { app, BrowserWindow, screen, Menu, Tray, nativeImage } from 'electron';
+import { app, BrowserWindow, screen, Menu, Tray, nativeImage, dialog } from 'electron';
+const { autoUpdater } = require("electron-updater");
 import * as path from 'path';
 import * as url from 'url';
 import * as AutoLaunch from 'auto-launch';
+
+function sendStatusToWindow(text) {
+  win.webContents.send('message', text);
+
+  const options = {
+    type: 'info',
+    buttons: ['OK'],
+    // defaultId: 2,
+    // title: 'Question',
+    message: text,
+    // detail: 'It does not really matter',
+    // checkboxLabel: 'Remember my answer',
+    // checkboxChecked: true,
+  };
+
+  dialog.showMessageBox(win, options, (response, checkboxChecked) => {
+    console.log(response);
+    console.log(checkboxChecked);
+  });
+}
+
+// checking for app newer version
+function checkForUpdates() {
+  autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+  })
+
+  autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded, quitting the app to launch the updates!');
+    setTimeout(() => {
+      autoUpdater.quitAndInstall();
+    }, 5000);
+  });
+
+  autoUpdater.checkForUpdatesAndNotify();
+}
 
 // auto launch on OS startup, skipped in development
 function enableAutoLaunch() {
@@ -37,7 +74,7 @@ function enableAutoLaunch() {
 function createTray() {
   let tray = null;
   const icon = nativeImage.createFromPath(path.join(__dirname, 'bilbeoGA.png'));
-  
+
   tray = new Tray(icon);
   const contextMenu = Menu.buildFromTemplate([
     {
@@ -106,8 +143,10 @@ function createWindow() {
     }));
   }
 
+  // win.webContents.openDevTools();
   if (serve) {
     win.webContents.openDevTools();
+    
   }
 
   // Emitted when the window is closed.
@@ -120,33 +159,54 @@ function createWindow() {
 }
 
 try {
+  const gotTheLock = app.requestSingleInstanceLock();
+  console.log("gotTheLock", gotTheLock)
+  if (!gotTheLock) {
+    app.quit();
+  } else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+      // Someone tried to run a second instance, we should focus our window.
+      if (win) {
+        console.log("win", win.isMinimized())
+        if (win.isMinimized()) {
+          win.restore()
+        }
+        win.focus();
+      }
+    })
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', ()=>{
-    createWindow();
-    createTray();
-  });
-
-  enableAutoLaunch();
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.on('ready', () => {
       createWindow();
-    }
-  });
+      createTray();
+      checkForUpdates();
+    });
+
+    enableAutoLaunch();
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', () => {
+      // On OS X it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (process.platform !== 'darwin') {
+        app.quit();
+      }
+    });
+
+    app.on('activate', () => {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (win === null) {
+        createWindow();
+      }
+    });
+
+    app.on('before-quit', function () {
+      app['isQuiting'] = true;
+    });
+  }
 
 } catch (e) {
   // Catch Error
