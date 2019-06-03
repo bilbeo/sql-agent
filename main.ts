c130c63f38ac5494143128436306bd3e0569825a
sat-m <markosyansatenik@gmail.com>
2019-06-03 18:17:24 +0400

    silent autolaunch issue solved

--- main.ts
+++ main.ts
@@ -72,8 +72,6 @@ function enableAutoLaunch() {
     });
 }
 
-
-
 function createWindow() {
 
   const electronScreen = screen;
@@ -103,10 +101,8 @@ function createWindow() {
       slashes: true
     }));
   }
-
   if (serve) {
     win.webContents.openDevTools();
-
   }
 
   // Emitted when the window is closed.
@@ -116,6 +112,15 @@ function createWindow() {
     // when you should delete the corresponding element.
     win = null;
   });
+
+  // when app is launched on system startup, launch it as minimzed
+  const startMinimized = args.some(val => val === '--hidden');
+  if (startMinimized == true) {
+    win.hide();
+    logger.info('App is started by AutoLaunch');
+  } else {
+    logger.info('App is started by User');
+  }
 }
 
 try {
